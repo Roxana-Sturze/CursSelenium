2c8f674e97ade1ba0ee4e18877370c227cc3a8d3
Roxana Sturze <roxanagabrielasturze@gmail.com>
2023-10-26 21:29:43 +0300

    curs16 final

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
@@ -13,6 +15,7 @@ public class BaseTest {
 	public void setup() {
 		
 		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize(); //maximizes the browser window
 		driver.get("https://keybooks.ro"); // navigates to the specified URL
 		
