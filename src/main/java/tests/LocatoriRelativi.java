1bd9e47b6670320ed349766d4eeb5b9ed823112e
Roxana Sturze <roxanagabrielasturze@gmail.com>
2023-11-06 21:37:02 +0200

    curs19 final

--- src/main/java/tests/LocatoriRelativi.java
+++ src/main/java/tests/LocatoriRelativi.java
@@ -24,12 +24,30 @@ public class LocatoriRelativi extends BaseTest{
 		
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		
+		
+		//to left of
 		WebElement price = driver.findElement(RelativeLocator.with(By.xpath("//h5[contains(@class, 'sc_title')]")).toLeftOf(By.xpath("//a[@href= 'life-in-the-garden']")));
 		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red')", price );
 		
+		
+		//to right of
 		WebElement price2 = driver.findElement(RelativeLocator.with(By.xpath("//h5[contains(@class, 'sc_title')]")).toRightOf(By.xpath("//a[@href= 'life-in-the-garden']")));
 		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red')", price2 );
 		
+		
+		//below
+		WebElement price3 = driver.findElement(RelativeLocator.with(By.xpath("//h5[contains(@class, 'sc_title')]")).below(By.xpath("//a[@href= 'life-in-the-garden']")));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red')", price3 );
+		
+		//above
+		WebElement authorMann = driver.findElement(RelativeLocator.with(By.xpath("//p[text()='Richard Mann']")).above(By.xpath("//h5[contains(@class, 'sc_title')]")));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red')", authorMann );
+		
+		//near
+		WebElement authorMann2 = driver.findElement(RelativeLocator.with(By.xpath("//p[text()='Richard Mann']")).near(By.xpath("//a[@href= 'life-in-the-garden']"), 100));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red')", authorMann2 );
+		
+				
 		
 	}
 
